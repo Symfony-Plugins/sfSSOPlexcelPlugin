a4a79852c3d0b39f0f3743bb5e9b58dea614b23a
hartym <hartym@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-02-24 07:57:31 -0600

    [sfSSOPlexcelPlugin] renamed configuration class
    git-svn-id: http://svn.symfony-project.com/plugins/sfSSOPlexcelPlugin/trunk@15750 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/sfSSOPlexcelPluginConfiguration.class.php
+++ config/sfSSOPlexcelPluginConfiguration.class.php
@@ -2,13 +2,13 @@
 
 /**
  * sfPlexcelPlugin configuration.
- * 
+ *
  * @package     sfPlexcelPlugin
  * @subpackage  config
  * @author      Pierre Cahard <pcahard@gmail.com>
  * @version     SVN: $Id: PluginConfiguration.class.php 12628 2008-11-04 14:43:36Z Kris.Wallsmith $
  */
-class sfPlexcelPluginConfiguration extends sfPluginConfiguration
+class sfSSOPlexcelPluginConfiguration extends sfPluginConfiguration
 {
   /**
    * @see sfPluginConfiguration
