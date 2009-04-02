9f965114c4076612a255074623eebcd3dd6d9373
pcahard <pcahard@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-04-02 17:45:25 -0500

    [sfSSOPlexcelForm] updated login form with a hidden field for sso
    git-svn-id: http://svn.symfony-project.com/plugins/sfSSOPlexcelPlugin/trunk@16922 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/form/base/BaseSSOPlexcelForm.class.php
+++ lib/form/base/BaseSSOPlexcelForm.class.php
@@ -5,6 +5,10 @@ class BaseSSOPlexcelForm extends BasesfGuardFormSignin
   public function setup()
   {
     parent::setup();
+
+    $this->widgetSchema['sso_login']    = new sfWidgetFormInputHidden();
+    $this->validatorSchema['sso_login'] = new sfValidatorBoolean();
+
     // change the default Post validator
     $this->validatorSchema->setPostValidator(new sfSSOValidatorUser());
   }
